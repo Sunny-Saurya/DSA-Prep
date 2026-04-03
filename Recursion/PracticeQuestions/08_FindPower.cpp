791cc5862110425ae9825d7c0d29060dbf463948
Sunny Kumar <sunnysaurya9955@gmail.com>
2026-04-03 20:03:17 +0530

    Find power

--- Recursion/PracticeQuestions/08_FindPower.cpp
+++ Recursion/PracticeQuestions/08_FindPower.cpp
@@ -4,10 +4,28 @@ using namespace std;
 int power(int n, int x){
     // base case
     if(x== 0) return 1;
+
     long long ans = n * power(n, x-1);
     return ans;
 }
 
+// optimized 
+
+int power2(int n, int x){
+    
+    // bc
+
+    if(x == 0) return 1;
+
+    int half = power2(n, x/2);
+
+    if(x % 2 == 0){
+        return half * half;
+    }
+    else{
+        return n*half*half;
+    }
+}
 int main(){
     int n;
     cout << "Enter Number : ";
@@ -17,7 +35,7 @@ int main(){
     cout << "Power : ";
     cin >> x;
 
-    cout << power(n,x) << endl;
+    cout << power2(n,x) << endl;
     return 0;
 
 }
